97179b33df4240093dbdead18a0e85660dbc958a
Alpian <Nova07pplg@gmail.com>
2024-11-22 09:39:46 +0800

    initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        ComboBox1 = New ComboBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        DataGridView1 = New DataGridView()
+        Button4 = New Button()
+        Button3 = New Button()
+        Button5 = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(37, 35)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(105, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Form Anggota"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(37, 281)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(57, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Alamat"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(37, 225)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(44, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Kelas"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(37, 165)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(49, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Nama"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(37, 100)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(91, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "No Anggota"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(37, 466)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(53, 20)
+        Label6.TabIndex = 5
+        Label6.Text = "No Hp"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(37, 346)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(57, 20)
+        Label7.TabIndex = 6
+        Label7.Text = "Jurusan"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(37, 405)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(46, 20)
+        Label8.TabIndex = 7
+        Label8.Text = "Email"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(149, 97)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 8
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(140, 281)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 9
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(149, 162)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 27)
+        TextBox4.TabIndex = 11
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(140, 402)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(125, 27)
+        TextBox5.TabIndex = 12
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(140, 343)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(125, 27)
+        TextBox6.TabIndex = 13
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(140, 466)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(125, 27)
+        TextBox7.TabIndex = 14
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.AccessibleDescription = ""
+        ComboBox1.AccessibleName = ""
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"XI PPLG 1", "XI PPLG 2"})
+        ComboBox1.Location = New Point(140, 217)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 15
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(37, 529)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 16
+        Button1.Text = "Simpan"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(161, 529)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 17
+        Button2.Text = "Keluar"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(422, 35)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(949, 467)
+        DataGridView1.TabIndex = 18
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(503, 529)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 20
+        Button4.Text = "Test"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(290, 529)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 21
+        Button3.Text = "Bersih"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(390, 529)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 29)
+        Button5.TabIndex = 22
+        Button5.Text = "DarkMode"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1402, 644)
+        Controls.Add(Button5)
+        Controls.Add(Button3)
+        Controls.Add(Button4)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button5 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,279 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+
+    Private counter As Integer = 1
+    Private isDarkMode As Boolean = False
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        Dim conn As MySqlConnection
+        Dim COMMAND As MySqlCommand
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost;userid=root;password='';database=data_anggota"
+
+        Try
+            conn.Open()
+            MessageBox.Show("Connection to MySQL test database was successful!!!!", "TESTING      CONNECTION TO MySQL DATABASE")
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set KeyPreview to true
+        Me.KeyPreview = True
+
+        ' Refresh data grid view saat form dimuat
+        RefreshDataGridView()
+
+        TextBox1.Text = counter.ToString()
+        TextBox1.ReadOnly = True
+
+        ' Isi data ke dalam combobox
+        FillComboBoxData()
+
+        ' Deteksi waktu lokal dan aktifkan mode gelap jika diperlukan
+        SetupDarkMode()
+    End Sub
+
+    Private Sub SetupDarkMode()
+        Dim localTime As DateTime = DateTime.Now
+        Dim centralIndonesiaTime As DateTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(localTime, "SE Asia Standard Time")
+
+        ' Mode gelap otomatis diaktifkan antara pukul 18:00 dan 06:00
+        If centralIndonesiaTime.Hour >= 18 OrElse centralIndonesiaTime.Hour < 6 Then
+            EnableDarkMode()
+        Else
+            EnableLightMode()
+        End If
+    End Sub
+
+    Private Sub EnableDarkMode()
+        BackColor = Color.FromArgb(40, 40, 40)
+        For Each ctrl As Control In Controls
+            ctrl.BackColor = Color.FromArgb(40, 40, 40)
+            ctrl.ForeColor = Color.White
+        Next
+        Button5.Text = "Light Mode"
+        isDarkMode = True
+    End Sub
+
+    Private Sub EnableLightMode()
+        BackColor = SystemColors.Control
+        For Each ctrl As Control In Controls
+            ctrl.BackColor = SystemColors.Window
+            ctrl.ForeColor = SystemColors.ControlText
+        Next
+        Button5.Text = "Dark Mode"
+        isDarkMode = False
+    End Sub
+
+    ' Menangani event KeyDown untuk menambahkan shortcut keyboard
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        ' Menangani shortcut keyboard
+        If e.Control AndAlso e.KeyCode = Keys.S Then
+            e.SuppressKeyPress = True ' Mencegah default behavior (Select All)
+            Button1.PerformClick() ' Ctrl + S untuk penambahan data
+        ElseIf e.Control AndAlso e.KeyCode = Keys.O Then
+            e.SuppressKeyPress = True ' Mencegah default behavior
+            Button2.PerformClick() ' Ctrl + O untuk Keluar
+        ElseIf e.Control AndAlso e.KeyCode = Keys.C Then
+            e.SuppressKeyPress = True ' Mencegah default behavior
+            Button3.PerformClick() ' Ctrl + C untuk pengosongan kolom
+        ElseIf e.Control AndAlso e.KeyCode = Keys.D Then
+            e.SuppressKeyPress = True ' Mencegah default behavior
+            Button5.PerformClick() ' Ctrl + D untuk mengganti Tema
+        End If
+    End Sub
+
+    Private Sub FillComboBoxData()
+        ComboBox1.Items.Add("XI PPLG 1")
+        ComboBox1.Items.Add("XI RPL 1")
+        ComboBox1.Items.Add("XII TKJ 2")
+
+        ' Atur combobox agar hanya menampilkan opsi saat diklik
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ' Kode untuk menghandle event saat combobox dipilih
+    End Sub
+
+    Private Sub TextBox7_TextChanged(sender As Object, e As EventArgs) Handles TextBox7.TextChanged
+        ' Hanya menerima input angka, maksimal 13 digit
+        Dim input As String = TextBox7.Text
+        If input.Length > 13 Then
+            ' Potong input menjadi 13 digit
+            TextBox7.Text = input.Substring(0, 13)
+            ' Tempatkan kursor di akhir teks
+            TextBox7.SelectionStart = TextBox7.Text.Length
+        ElseIf Not IsNumeric(input) Then
+            ' Hapus karakter non-angka
+            TextBox7.Text = input.Replace(input.Last, "")
+            ' Tempatkan kursor di akhir teks
+            TextBox7.SelectionStart = TextBox7.Text.Length
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Validasi input
+        If String.IsNullOrEmpty(TextBox1.Text) Or String.IsNullOrEmpty(TextBox2.Text) Or String.IsNullOrEmpty(ComboBox1.Text) Or String.IsNullOrEmpty(TextBox4.Text) Or String.IsNullOrEmpty(TextBox5.Text) Or String.IsNullOrEmpty(TextBox6.Text) Or String.IsNullOrEmpty(TextBox7.Text) Then
+            MessageBox.Show("Semua kolom harus diisi.")
+            Return
+        End If
+
+        ' Validasi nomor HP
+        Dim noHP As String = TextBox7.Text
+        If noHP.Length <> 13 OrElse Not IsNumeric(noHP) Then
+            MessageBox.Show("Nomor HP harus terdiri dari 13 angka.")
+            Return
+        End If
+
+        ' Validasi apakah nomor anggota sudah digunakan
+        Dim noAnggota As String = TextBox1.Text
+        If IsNoAnggotaUsed(noAnggota) Then
+            MessageBox.Show("Nomor anggota sudah digunakan.")
+            Return
+        End If
+
+        ' Konfirmasi penambahan data
+        Dim result As DialogResult = MessageBox.Show("Apakah Kamu Ingin Menambahkan Data Ini?", "Konfirmasi Penambahan", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.No Then
+            Return
+        End If
+
+        ' Simpan data ke database
+        Dim conn As New MySqlConnection("server=localhost;userid=root;password='';database=data_anggota")
+        Dim query As String = "INSERT INTO anggota (no_anggota, nama, kelas, alamat, jurusan, email, no_hp) VALUES (@no_anggota, @nama, @kelas, @alamat, @jurusan, @email, @no_hp)"
+        Dim cmd As New MySqlCommand(query, conn)
+
+        ' Ambil nilai dari kontrol input
+        cmd.Parameters.AddWithValue("@no_anggota", TextBox1.Text)
+        cmd.Parameters.AddWithValue("@nama", TextBox2.Text)
+        cmd.Parameters.AddWithValue("@kelas", ComboBox1.SelectedItem)
+        cmd.Parameters.AddWithValue("@alamat", TextBox4.Text)
+        cmd.Parameters.AddWithValue("@jurusan", TextBox5.Text)
+        cmd.Parameters.AddWithValue("@email", TextBox6.Text)
+
+        ' Sensor nomor HP dengan bintang
+        Dim sensoredNoHP As String = New String("*"c, noHP.Length)
+        cmd.Parameters.AddWithValue("@no_hp", sensoredNoHP)
+
+        Try
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Data berhasil disimpan.")
+
+            ' Refresh data grid view
+            RefreshDataGridView()
+        Catch ex As Exception
+            MessageBox.Show("Gagal menyimpan data: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+
+        counter += 1
+        TextBox1.Text = counter.ToString()
+    End Sub
+
+    Private Sub RefreshDataGridView()
+        Dim conn As New MySqlConnection("server=localhost;userid=root;password='';database=data_anggota")
+        Dim adapter As New MySqlDataAdapter("SELECT * FROM anggota", conn)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Konfirmasi keluar dari aplikasi
+        Dim result As DialogResult = MessageBox.Show("Anda yakin ingin keluar dari aplikasi?", "Konfirmasi Keluar", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Function IsValidEmail(email As String) As Boolean
+        Dim emailRegex As New System.Text.RegularExpressions.Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$")
+        Return emailRegex.IsMatch(email)
+    End Function
+
+    Private Function IsNoAnggotaUsed(noAnggota As String) As Boolean
+        Dim conn As New MySqlConnection("server=localhost;userid=root;password='';database=data_anggota")
+        Dim query As String = "SELECT COUNT(*) FROM anggota WHERE no_anggota = @no_anggota"
+        Dim cmd As New MySqlCommand(query, conn)
+        cmd.Parameters.AddWithValue("@no_anggota", noAnggota)
+
+        Try
+            conn.Open()
+            Dim count As Integer = Convert.ToInt32(cmd.ExecuteScalar())
+            Return count > 0
+        Catch ex As Exception
+            MessageBox.Show("Gagal memeriksa nomor anggota: " & ex.Message)
+            Return True
+        Finally
+            conn.Close()
+        End Try
+    End Function
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Kosongkan semua kolom kecuali kolom No Anggota
+        TextBox2.Text = ""
+        ComboBox1.SelectedIndex = -1
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' Periksa mode saat ini
+        If Me.BackColor = SystemColors.Control Then
+            ' Mode terang saat ini, ubah ke mode gelap
+            Me.BackColor = Color.FromArgb(35, 35, 35)
+            Me.ForeColor = Color.White
+            Button1.BackColor = Color.FromArgb(64, 64, 64)
+            Button2.BackColor = Color.FromArgb(64, 64, 64)
+            Button3.BackColor = Color.FromArgb(64, 64, 64)
+            Button4.BackColor = Color.FromArgb(64, 64, 64)
+            Button5.BackColor = Color.FromArgb(64, 64, 64)
+            TextBox1.BackColor = Color.FromArgb(64, 64, 64)
+            TextBox2.BackColor = Color.FromArgb(64, 64, 64)
+            TextBox4.BackColor = Color.FromArgb(64, 64, 64)
+            TextBox5.BackColor = Color.FromArgb(64, 64, 64)
+            TextBox6.BackColor = Color.FromArgb(64, 64, 64)
+            TextBox7.BackColor = Color.FromArgb(64, 64, 64)
+            ComboBox1.BackColor = Color.FromArgb(64, 64, 64)
+            ComboBox1.ForeColor = Color.White
+            DataGridView1.BackgroundColor = Color.FromArgb(64, 64, 64)
+            DataGridView1.ForeColor = Color.White
+        Else
+            ' Mode gelap saat ini, ubah ke mode terang
+            Me.BackColor = SystemColors.Control
+            Me.ForeColor = SystemColors.ControlText
+            Button1.BackColor = SystemColors.Control
+            Button2.BackColor = SystemColors.Control
+            Button3.BackColor = SystemColors.Control
+            Button4.BackColor = SystemColors.Control
+            Button5.BackColor = SystemColors.Control
+            TextBox1.BackColor = SystemColors.Window
+            TextBox2.BackColor = SystemColors.Window
+            TextBox4.BackColor = SystemColors.Window
+            TextBox5.BackColor = SystemColors.Window
+            TextBox6.BackColor = SystemColors.Window
+            TextBox7.BackColor = SystemColors.Window
+            ComboBox1.BackColor = SystemColors.Window
+            ComboBox1.ForeColor = SystemColors.ControlText
+            DataGridView1.BackgroundColor = SystemColors.Window
+            DataGridView1.ForeColor = SystemColors.ControlText
+        End If
+
+        If isDarkMode Then
+            EnableLightMode()
+        Else
+            EnableDarkMode()
+        End If
+
+    End Sub
+End Class
